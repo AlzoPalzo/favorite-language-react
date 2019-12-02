e5354c63c287c89d058c4df4d74ecb5ec9c0ef83
AlzoPalzo <555madni@gmail.com>
2019-12-02 01:05:55 +0000

    created components

---
+++ src/components/Home.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import Account from "./Account/Accounts";
+import AddAccount from "./Account/AddAccount";
+
+class Home extends Component {
+    state = { 
+        accounts: [],
+        page: 'home'
+     }
+    render() { 
+        return (
+        
+          <div>
+
+          </div>  
+        );
+    }
+}
+ 
+export default Home;
